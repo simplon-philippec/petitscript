e58c6b745b126672f844318681e7e9e4eb5e1ee6
simplon-philippec <colas.p@hotmail.fr>
2016-12-22 01:10:06 +0100

    Ajout index et script

---
+++ script.js
@@ -0,0 +1,14 @@
+// Projet : Créer un verificateur / annonceur de syntaxe.
+
+//1.  : Verification du fichier
+//1.1 : Parse du fichier
+
+
+
+
+//1.2 : Tableau avec toutes les chaines de caracteres de variables/functions/paramettres
+//1.3 : Verifie les doublons insensible à la casse et les gardes
+//1.4 : Verifie les doublons différents sensible ou non à la casse
+//1.5 : Affichage de la ligne et du mots en questions
+
+//2.  : Affichage de la ligne et du mots mal écrit
